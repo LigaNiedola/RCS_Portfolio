eb91e57d02c5bff458a841e5520add0fa3ca82bb
Liga Niedola <niedola.liga@gmail.com>
2023-08-28 19:16:51 +0300

    Contact info

--- inc/contact.php
+++ inc/contact.php
@@ -20,11 +20,29 @@ echo'
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="assets/css/style.css"/>
         <title>Līga Niedola | Web Developer</title>
+        <style>
+            .contact {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                min-height: 100vh;
+                margin: 0;
+            }
+
+            .container_info {
+                text-align: left;
+                padding: 20px;
+                border: 1px solid #ccc;
+                max-width: 600px;
+                width: 100%;
+            }
+        </style>
     </head>
     <body>
         <section class="contact" id="contact">
-        <div>
-            <h2>Thank you for contacting me!I will get back to you as soon as possible.</h2>
+        <div class="container">
+            <h2>Thank you for contacting me! <br>
+            I will get back to you as soon as possible.</h2>
             <p>Go back to the <a href="https://liganiedola.000webhostapp.com">homepage</a></p>
         </div>
         </section>
